19515d573f691431dbacbbb80a1e255e867fb178
ipopop <popop@ntymail.com>
2021-09-08 20:09:20 +0200

    add a README.md

---
+++ README.md
@@ -0,0 +1,25 @@
+<h1 align="center">Just a few simple exercises in Javascript 👋</h1>
+
+### 🚀 [My Live Page](https://ipopop.github.io/dyma-fr-revision-JS/)
+--- 
+
+> thank's to '[Dyma.fr](https://dyma.fr)' for the lessons
+---
+
+<img src="./assets/img/screen-01.jpg" width=300/>
+
+    .
+    ├── assets
+    │   └── img
+    │       ├── img-01.jpg
+    │       └── img-02.jpg
+    ├── src
+    │   ├── css
+    │   │   ├── main.css
+    │   │   └── todo.css
+    │   └── js
+    │       ├── asyncAndTimers.js
+    │       ├── dom.js
+    │       ├── index.js
+    │       └── todo.js
+    └── index.html
--- src/js/asyncAndTimers.js
+++ src/js/asyncAndTimers.js
@@ -15,13 +15,13 @@ newSpan.innerText = 1
 let idx = 0
 const delay = 500
 
-const intervalAndTimer = (() => {  
+const intervalAndTimer = (() => {
   const newInterval = setInterval(() => {
-    
+
     const newTimeout = setTimeout(() => {
       newSpan.innerText = idx
     }, delay)
-  
+
     idx++
     console.log('index :', idx)
 
@@ -49,7 +49,7 @@ intervalAndTimer()
 const unePromesse3 = new Promise((resolve, reject) => {
   resolve(42)
   reject("erreur...") // est ignoré
-  resolve(22)         // est ignoré
+  resolve(22) // est ignoré
 })
 
 // la méthode '.then()'
@@ -58,21 +58,20 @@ const unePromesse4 = new Promise((resolve, reject) => {
 })
 
 unePromesse4.then(
-  resultat => console.log('promise 4 :', resultat),  // 42
-  erreur =>  console.log(erreur) 
+  resultat => console.log('promise 4 :', resultat), // 42
+  erreur => console.log(erreur)
 )
 
 // la méthode 'catch' (un 'then' pour les erreurs)
 // unePromesse4.catch(erreur => { console.log(erreur) })
 
 const newPromise = new Promise(resolve => {
-  setTimeout(() => resolve(22), 1000)
+    setTimeout(() => resolve(22), 1000)
   })
   .then(res => { /* code */ })
   .then(res => { /* code */ })
   .then(res => { /* code */ })
-  .catch(err => console.error(err.message)
-  )
+  .catch(err => console.error(err.message))
 
 // les méthodes des promesses
 const p1 = new Promise((resolve, reject) => {
@@ -95,20 +94,84 @@ const p3 = new Promise((resolve, reject) => {
 })
 
 Promise.all([p1, p2, p3])
-  .then(result => console.log('Promise.all :', result)) 
+  .then(result => console.log('Promise.all :', result))
   .catch(err => console.log('Promise.all error :', err))
-  // Promise.all error : 2
-  
-  Promise.allSettled([p1, p2, p3])
+// Promise.all error : 2
+
+Promise.allSettled([p1, p2, p3])
   .then(result => console.log('Promise.allSettled :', result))
   .catch(err => console.log('Promise.allSettled error :', err))
-  /* Array(3) [ {…}, {…}, {…} ]
-  0: Object { status: "fulfilled", value: 1 }
-  1: Object { status: "rejected", reason: 2 }
-  2: Object { status: "fulfilled", value: 3 }
-  */
- 
- Promise.race([p1, p2, p3])
-   .then(result => console.log('Promise.race :', result)) 
-   .catch(err => console.log('Promise.race error :', err))
-   // Promise.race : 3
+/* Array(3) [ {…}, {…}, {…} ]
+0: Object { status: "fulfilled", value: 1 }
+1: Object { status: "rejected", reason: 2 }
+2: Object { status: "fulfilled", value: 3 }
+*/
+
+Promise.race([p1, p2, p3])
+  .then(result => console.log('Promise.race :', result))
+  .catch(err => console.log('Promise.race error :', err))
+// Promise.race : 3
+
+
+// - async / await  -
+
+const promx = Promise.reject("Async/Await : Error Msg")
+const fx = async () => {
+  try {
+    const valeur = await promx
+  } catch (err) {
+    console.error(err)
+  }
+}
+fx()
+
+// sequentiel
+const sp1 = () => new Promise(resolve => setTimeout(() => resolve(42), 5000))
+const sp2 = () => new Promise(resolve => setTimeout(() => resolve(12), 3000))
+
+async function sequentiel() {
+  try {
+    const val1 = await sp1()
+    console.log("sequentiel1", val1)
+    const val2 = await sp2()
+    console.log("sequentiel2", val2)
+  } catch (err) {
+    console.log(err)
+  }
+}
+sequentiel()
+
+// concurent
+const cp1 = () => new Promise(resolve => setTimeout(() => resolve(42), 5000))
+const cp2 = () => new Promise(resolve => setTimeout(() => resolve(12), 3000))
+
+async function concurrent() {
+  try {
+    const val1 = await cp1()
+    console.log("concurrent1", val1)
+    const val2 = await cp2()
+    console.log("concurrent2", val2)
+  } catch (err) {
+    console.log(err)
+  }
+}
+concurrent()
+
+
+// parallel
+const pf1 = () => new Promise(resolve => setTimeout(() => resolve(42), 5000))
+const pf2 = () => new Promise(resolve => setTimeout(() => resolve(12), 3000))
+
+async function parrallel() {
+  try {
+    const [val1, val2] = await Promise.all([pf1(), pf2()])
+    console.log("parrallel2", val2)
+    console.log("parrallel1", val1)
+  } catch (err) {
+    console.log(err)
+  }
+}
+parrallel()
+
+
+// - event loop -
